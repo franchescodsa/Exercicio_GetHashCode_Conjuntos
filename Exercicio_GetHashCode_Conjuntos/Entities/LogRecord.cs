7ea5677a020b0b019577a70821259a8248d68f8d
franchesco aprigio <franchescodsa@outlook.com>
2024-08-06 22:35:36 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_GetHashCode_Conjuntos/Entities/LogRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_GetHashCode_Conjuntos.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public  DateTime Instant { get; set; }
+
+        public LogRecord(string username, DateTime instant)
+        {
+            Username = username;
+            Instant = instant;
+        }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
